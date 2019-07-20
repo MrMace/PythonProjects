cbb1e742a7b3fd4ab45f65288945608616d74287
Matt <mace.matt@umail.iu.edu>
2019-07-20 12:50:21 -0500

    Create recursionSimpleReverse.py

---
+++ recursionSimpleReverse.py
@@ -0,0 +1,9 @@
+numList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+def reverse(numList):
+     #base case return  if we reach end. 
+    if len(numList) == 0:
+        return numList      
+    else:
+        return reverse(numList[1:]) + numList[:1]
+print("Number List Regular: {}".format(numList))
+print("The List Reversed: {}".format(reverse(numList)))

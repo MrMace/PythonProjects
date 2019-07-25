05adfe3a162a715b1003e6f6a007305f6258a485
MrMace <mrmace@iu.edu>
2019-07-25 16:06:11 -0400

    Update QueueSimple.py

--- QueueSimple.py
+++ QueueSimple.py
@@ -1,23 +1,33 @@
 class Queue:
     
+    #construct the list
     def __init__(self):
         self.items = []
         
+     #check if empty   
     def emptyQ(self):
         return self.items == []
     
+    #insert into queue
     def enqueue(self,item):
         self.items.insert(0,item)
         
+     #remove from queue   
     def dequeue(self):
         self.items.pop()
         
+    # returns size of list   
     def size(self):
         return len(self.items)
     
+    #prints out each item
     def printqueue(self):
         for items in self.items:
             print(items)
+    
+    #prints out list
+    def viewList(self):
+        return self.items
             
 myQ = Queue()
 print(myQ.emptyQ())
@@ -27,3 +37,5 @@ myQ.enqueue("Im Last")
 myQ.printqueue()
 myQ.dequeue()
 myQ.printqueue()
+print(myQ.emptyQ())
+print(myQ.viewList())

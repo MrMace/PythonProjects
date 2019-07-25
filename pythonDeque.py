1139963796d26500b00ccd4487cda4c6876980c8
MrMace <mrmace@iu.edu>
2019-07-25 16:07:43 -0400

    Update pythonDeque.py

--- pythonDeque.py
+++ pythonDeque.py
@@ -1,26 +1,34 @@
 class Deque:
     
+    #Constructor
     def __init__(self):
         self.items = []
         
+    #Checks if empty  
     def isEmpty(self):
         return self.items == []
     
+    #adds to the front
     def addFront(self, item):
         self.items.append(item)
         
+     #adds to back   
     def addBack(self, item):
         self.items.insert(0,item)
         
+     #removes from front   
     def removeFront(self):
         return self.items.pop()
     
+    #removes from back
     def removeBack(self):
         return self.items.pop(0)
     
+    #returns size
     def size(self):
         return len(self.items)
     
+    #returns list contents
     def viewList(self):
         return self.items
     
@@ -30,6 +38,9 @@ dQ = Deque()
 dQ.addFront("FrontOne")
 dQ.addFront("SecondFront")
 dQ.addBack("Added Back")
+dQ.addBack("Second Back")
 print(dQ.size())
 print(dQ.viewList())
-
+print(dQ.isEmpty())
+dQ.addFront("Added")
+print(dQ.viewList())

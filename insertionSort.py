b8b83a6707a60bab437dc06d9729a590fa10e2f8
Matt <mrmace@umail.iu.edu>
2019-08-03 16:16:37 -0400

    !

--- insertionSort.py
+++ insertionSort.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-#List given 
+#ListGiven
 givenList =  [1,2,3,5,4,6,7,8,9,10]
 
 '''
@@ -28,37 +27,3 @@ insertionSort(givenList)
 
 #prints out to our console the list. 
 print("The sorted list is: {} ".format(givenList))
-=======
-#List given 
-givenList =  [1,2,3,5,4,6,7,8,9,10]
-
-
-'''
-This is the Insertion sorting algorithm 
-'''
-
-def insertionSort(givenList): #Function
-    
-    listLen = len(givenList) #gets length of the list
-    
-    for i in range(1, listLen): #goes through 1 and length of list
-        
-        key = givenList[i]
-        
-        
-        #Move through list, anything greater than key is moved
-        #ahead one position.
-        j = i - 1
-        
-        while j >= 0 and key < givenList[j]:
-            givenList[j + 1] = givenList[j]
-            j -= 1
-        givenList[j + 1] = key
-
-insertionSort(givenList)
-
-#prints out to our console the list. 
-print("The sorted list is: {} ".format(givenList))
-        
-        
->>>>>>> ee95c9602ad586e4f878df49fe1fe6e169e9613b

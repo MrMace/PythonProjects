b892dde5f31f01d9d4d016e38c0f318a44b13f8a
Matt <mrmace@umail.iu.edu>
2019-08-03 16:13:12 -0400

    Update hashExample.py

--- hashExample.py
+++ hashExample.py
@@ -1,3 +1,9 @@
+#Dictionaries is a hash table implementation it uses key value stores and it is not ordered. Where a list is ordered
+#and instead of using key values stores the values are indexed.
+#With list you have to enclose it with []. With a dictionary it is enclosed with {}.
+#Items can be removed using almost the same syntax operator del list[index] for the list and del dictionary[key].
+#According to our text books graphs List have 10 different methods where Dictionaries have half that of 5.
+
 class Person:
     def __init__(self, age, name): #initializer
         self.age = age
